ddb2837dffba6fde079bfceeac1bd2ac68e59ed7
JunH <101803254+Blaze-F@users.noreply.github.com>
2022-11-07 10:53:07 +0900

    [INIT] 초기설정

---
+++ ProjectName/user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from user.views import login, signup
+
+app_name = "user"
+urlpatterns = [
+    path("login/", login,name="login"),
+    path("signup/", signup,name="signup"),
+
+]
